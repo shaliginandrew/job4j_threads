dfe9cc95387edec39248723076b5c9c35c8590ac
shaliginandrew <shaligin.andrew@gmail.com>
2021-07-19 20:02:16 +0300

    Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -12,7 +12,7 @@ public class Wget {
                         }
                         System.out.println("Loaded.");
                     } catch (InterruptedException e) {
-                        e.printStackTrace();
+                        Thread.currentThread().interrupt();
                     }
                 }
         );
