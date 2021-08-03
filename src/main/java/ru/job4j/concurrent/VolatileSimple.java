b6b7068f466ca38b23a2e51940f38251452d0c8f
shaliginandrew <shaligin.andrew@gmail.com>
2021-08-03 22:00:38 +0300

    Volatile test

---
+++ src/main/java/ru/job4j/concurrent/VolatileSimple.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+import java.util.Scanner;
+
+public class VolatileSimple {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.start();
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        myThread.shutdown();
+    }
+}
+
+class MyThread extends Thread {
+    private volatile boolean running = true;
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown() {
+        this.running = false;
+    }
+}
