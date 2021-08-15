a1d85c5a9b0ea1267f3594d87b49d813affe34bf
shaliginandrew <shaligin.andrew@gmail.com>
2021-08-15 19:24:25 +0300

    Семафор Semaphore

---
+++ src/main/java/ru/job4j/concurrent/SemaphoreSimpleTwo.java
@@ -0,0 +1,62 @@
+package ru.job4j.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreSimpleTwo {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+        Connection connection = Connection.getConnection();
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+class Connection {
+    private static Connection connection = new Connection();
+    private int connectionCount;
+    private Semaphore semaphore = new Semaphore(9);
+
+    private Connection() {
+
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionCount++;
+            System.out.println(connectionCount);
+        }
+        Thread.sleep(5000);
+        synchronized (this) {
+            connectionCount--;
+        }
+    }
+}
+
